4b229e5dd5bb75ee15dcfbd702d52ae9340ce4c4
FedericoScuri <86447560+FedericoScuri@users.noreply.github.com>
2022-08-31 17:31:39 -0300

    tarea10

---
+++ sesion 10/index.js
@@ -0,0 +1,5 @@
+function suma(a,b){
+    return a+b
+}
+let sumas= suma(21,23)
+console.log(sumas)
---
+++ sesion 10/video-librerias/index.js
@@ -0,0 +1,15 @@
+
+import axios from "axios";
+
+axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+.then(function (response) {
+    // handle success
+    console.log(response.data);
+})
+.catch(function (error) {
+    // handle error
+    console.log(error);
+})
+.then(function () {
+    // always executed
+});
